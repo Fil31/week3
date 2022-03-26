7f5fb84c37fa1b763e7cb91aea4b42e3f5b38cdf
Fil31 <lolid76@gmail.com>
2022-03-26 17:02:35 +0300

    rabotaet

---
+++ GradingIf/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GradingIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your exam result");
+            char userInPut = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            if (userInPut == 'A')
+            {
+                Console.WriteLine("Outstanding");
+            }
+            else if (userInPut == 'B')
+            {
+                Console.WriteLine("Superior");
+            }
+            else if (userInPut == 'C')
+            {
+                Console.WriteLine("Good");
+            }
+            else if (userInPut == 'D')
+            {
+                Console.WriteLine("Satisfactory");
+            }
+            else if (userInPut == 'E')
+            {
+                Console.WriteLine("Low Pass");
+            }
+            else if (userInPut == 'F')
+            {
+                Console.WriteLine("Failure");
+            }
+            else
+            {
+                Console.WriteLine("This is not an exam result");
+            }
+        }
+    }
+    // Результат экзамена от A до F.
+    // A - outstanding B - Superior C - Good D - Satisfactory E - Low Pass F - Failure 
+}
---
+++ GradingSwitch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your exam result");
+            char userInPut = Convert.ToChar(Console.ReadLine().ToUpper());
+            switch(userInPut)
+            {
+                case 'A':
+                    Console.WriteLine("Outstanding!");
+                    break;
+                case 'B':
+                    Console.WriteLine("Superior");
+                    break;
+                case 'C':
+                    Console.WriteLine("Good");
+                    break;
+                case 'D':
+                    Console.WriteLine("Satisfactory");
+                    break;
+                case 'E':
+                    Console.WriteLine("Low pass");
+                    break;
+                case 'F':
+                    Console.WriteLine("Failure");
+                    break;
+            }
+        }
+        // Результат экзамена от A до F.
+        // A - outstanding B - Superior C - Good D - Satisfactory E - Low Pass F - Failure 
+    }
+}
